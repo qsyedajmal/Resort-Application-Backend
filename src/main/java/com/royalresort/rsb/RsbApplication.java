bfd6882706d7c06586312af8fe9354504272017a
Q SYED AJMAL <syedajmalq@gmail.com>
2023-08-30 01:11:34 +0530

    initialized project using required dependencies

---
+++ src/main/java/com/royalresort/rsb/RsbApplication.java
@@ -0,0 +1,13 @@
+package com.royalresort.rsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/royalresort/rsb/RsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.royalresort.rsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
